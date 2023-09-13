4761ea54909596a00ef5569cb00f3338d43f5f0e
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2023-09-13 05:10:04 +0000

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="display: flex;justify-content: center;">
+    <div class="weatherContainer">
+      <img :src="details.data.current.condition.icon" alt="">
+    <h1>{{ details.data.location.name }}</h1>
+    <p>{{ details.data.location.region }}</p>
+    <h1 class="c">{{ details.data.current.temp_c }} c</h1>
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      details: {},
+      inputCity: 'Nouakchot'
+    }
+  },
+  async created(){
+    await this.getData()
+    await console.log(this.details);
+  },
+  methods: {
+    async getData(){
+      this.details = await axios.get(`https://api.weatherapi.com/v1/current.json?key=5dd8525ecc1844fe8d841715231309&q=${this.inputCity}&aqi=yes`).then((res)=> res).catch((e)=> e);
+      this.details.data.current.temp_c = await parseInt(this.details.data.current.temp_c)
+    }
   }
 }
 </script>
@@ -22,5 +41,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.c{
+  font-size: 45px;
+}
+.weatherContainer{
+  background: #eeeeee52;
+  width: fit-content;
+  padding: 8px 45px;
+  border: 0px solid #2c3e50;
+  border-radius: 8px;
 }
 </style>
